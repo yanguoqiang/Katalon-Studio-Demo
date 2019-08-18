f48c2b7dc1ff2130caa8fc052ede4b717b161db1
yanguoqiang <yanguoqianghb@foxmail.com>
2019-08-18 00:45:47 +0000

    My first commit!

---
+++ Scripts/TC_make appointment/Script1563036834069.groovy
@@ -0,0 +1,38 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
+
+WebUI.click(findTestObject('Page_Index/a_Make Appointment'))
+
+WebUI.setText(findTestObject('Page_Login/input_Username_username'), 'John Doe')
+
+WebUI.setText(findTestObject('Page_Login/input_Password_password'), 'ThisIsNotAPassword')
+
+WebUI.click(findTestObject('Page_Login/button_Login'))
+
+WebUI.selectOptionByValue(findTestObject('Page_Appointment/select_Tokyo CURA Healthcare Center                            Hongkong CURA Healthcare Center                            Seoul CURA Healthcare Center'), 
+    'Hongkong CURA Healthcare Center', true)
+
+WebUI.setText(findTestObject('Page_Appointment/input_Visit Date (Required)_visit_date'), '01/08/2019')
+
+WebUI.setText(findTestObject('Page_Appointment/textarea_Comment_comment'), 'test')
+
+WebUI.click(findTestObject('Page_Appointment/button_Book Appointment'))
+
+WebUI.click(findTestObject('Page_Sumamry/a_Go to Homepage'))
+
